d5c2efa6da043e62da335a45d4b2dd11020f0b0f
wwagner4 <wwagner4@gmail.com>
2016-05-23 22:56:44 +0200

    new nois effects

--- src/main/java/net/entelijan/tryout/MyInst.java
+++ src/main/java/net/entelijan/tryout/MyInst.java
@@ -10,8 +10,11 @@ import ddf.minim.Minim;
 import ddf.minim.javasound.JSMinim;
 import ddf.minim.spi.MinimServiceProvider;
 import ddf.minim.ugens.ADSR;
-import ddf.minim.ugens.Damp;
 import ddf.minim.ugens.Instrument;
+import ddf.minim.ugens.MoogFilter;
+import ddf.minim.ugens.MoogFilter.Type;
+import ddf.minim.ugens.Noise;
+import ddf.minim.ugens.Noise.Tint;
 import ddf.minim.ugens.Oscil;
 import ddf.minim.ugens.Waves;
 
@@ -21,41 +24,8 @@ public class MyInst {
 		A, B;
 	}
 
-	private final String fileName = "myinst_03.wav";
+	private final String fileName = "myinst_08.wav";
 	private final boolean recording = false;
-
-	private static class InstA implements Instrument {
-
-		private AudioOutput out;
-		private double freq;
-
-		private Oscil toneOsc;
-
-		private Damp damp;
-
-		public InstA(AudioOutput out, double freq) {
-			super();
-			this.out = out;
-			this.freq = freq;
-			toneOsc = new Oscil(f(this.freq), 0.2f, Waves.TRIANGLE);
-			damp = new Damp(0.01f, 0.5f, 1.0f);
-
-			toneOsc.patch(damp);
-		}
-
-		@Override
-		public void noteOn(float duration) {
-			damp.patch(out);
-			damp.setDampTimeFromDuration(duration);
-			damp.activate();
-		}
-
-		@Override
-		public void noteOff() {
-			damp.unpatch(out);
-		}
-
-	}
 
 	private static class InstB implements Instrument {
 
@@ -71,7 +41,7 @@ public class MyInst {
 			this.out = out;
 			this.freq = freq;
 			toneOsc = new Oscil(f(this.freq), 0.2f, Waves.TRIANGLE);
-			adsr = new ADSR(2f, 0.01f, 0.7f, 0.1f, 1f);
+			adsr = new ADSR(1f, 0.001f, 0.1f, 0.03f, 1f);
 
 			toneOsc.patch(adsr);
 		}
@@ -90,35 +60,75 @@ public class MyInst {
 
 	}
 
+	private static class InstA implements Instrument {
+
+		private AudioOutput out;
+
+		private Noise noise;
+
+		private ADSR adsr;
+
+		private MoogFilter moog;
+
+		public InstA(AudioOutput out, double freq) {
+			super();
+			this.out = out;
+			noise = new Noise(Tint.PINK);
+			adsr = new ADSR(3f, 0.005f, 0.08f, 0.1f, 0.3f);
+			moog = new MoogFilter(f(freq), 0.5f, Type.BP);
+
+			noise.patch(moog).patch(adsr);
+		}
+
+		@Override
+		public void noteOn(float duration) {
+			adsr.noteOn();
+			adsr.patch(out);
+		}
+
+		@Override
+		public void noteOff() {
+			adsr.unpatchAfterRelease(out);
+			adsr.noteOff();
+		}
+
+	}
+
 	private void run(Ctx ctx) throws InterruptedException {
 
-		ctx.out.setTempo(100);
+		ctx.out.setTempo(200);
 		ctx.out.pauseNotes();
 
-		for (int i = 0; i < 10; i += 4) {
+		for (int i = 0; i < 23; i += 4) {
 			double baseDur = 0.5;
 			double baseDiff = 1.0;
-			double speedFac = 0.6;
+			double speedFac = 0.8;
 
-			double freq = 444;
-			double freqFac = 0.8;
+			double freq = 80;
+			double freqFac = 1.2;
 
-			playNote(i + 0, baseDur * r(4, ctx.ran), freq, Inst.A, ctx);
+			playNote(i + 0, baseDur * r(4, ctx.ran), freq, ranInst(ctx.ran), ctx);
 			double diff = baseDiff;
 			freq = freq * freqFac;
-			playNote(i + diff, baseDur * r(4, ctx.ran), freq, Inst.A, ctx);
+			playNote(i + diff, baseDur * r(4, ctx.ran), freq, ranInst(ctx.ran), ctx);
 			diff = baseDiff + diff * speedFac;
 			freq = freq * freqFac;
-			playNote(i + diff, baseDur * r(4, ctx.ran), freq, Inst.A, ctx);
+			playNote(i + diff, baseDur * r(4, ctx.ran), freq, ranInst(ctx.ran), ctx);
 			diff = baseDiff + diff * speedFac;
 			freq = freq * freqFac;
-			playNote(i + diff, baseDur * r(4, ctx.ran), freq, Inst.B, ctx);
+			playNote(i + diff, baseDur * r(4, ctx.ran), freq, ranInst(ctx.ran), ctx);
 			diff = baseDiff + diff * speedFac;
 			freq = freq * freqFac;
-			playNote(i + diff, baseDur * r(4, ctx.ran), freq, Inst.B, ctx);
+			playNote(i + diff, baseDur * r(4, ctx.ran), freq, ranInst(ctx.ran), ctx);
 			diff = baseDiff + diff * speedFac;
 			freq = freq * freqFac;
-			playNote(i + diff, baseDur * r(4, ctx.ran), freq, Inst.B, ctx);
+			playNote(i + diff, baseDur * r(4, ctx.ran), freq, ranInst(ctx.ran), ctx);
+			diff = baseDiff + diff * speedFac;
+			freq = freq * freqFac;
+			playNote(i + diff, baseDur * r(4, ctx.ran), freq, ranInst(ctx.ran), ctx);
+			diff = baseDiff + diff * speedFac;
+			freq = freq * freqFac;
+			playNote(i + diff, baseDur * r(4, ctx.ran), freq, ranInst(ctx.ran), ctx);
 		}
 
 		if (recording) {
@@ -126,6 +136,18 @@ public class MyInst {
 		}
 		ctx.out.resumeNotes();
 		waitAndClose(10, ctx);
+	}
+
+	private Inst ranInst(Random ran) {
+		int idx = ran.nextInt(2);
+		switch (idx) {
+		case 0:
+			return Inst.A;
+		case 1:
+			return Inst.B;
+		default:
+			throw new IllegalStateException("Unknown index " + idx);
+		}
 	}
 
 	private void playNote(double time, double dur, double fbase, Inst inst, Ctx ctx) {
