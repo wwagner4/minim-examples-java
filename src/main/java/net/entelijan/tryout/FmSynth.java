2a7b42ff6cbc69326349d432acddbb93b6c015a1
wwagner4 <wwagner4@gmail.com>
2016-05-30 08:05:36 +0200

    added inst 1 to fmsynth

--- src/main/java/net/entelijan/tryout/FmSynth.java
+++ src/main/java/net/entelijan/tryout/FmSynth.java
@@ -123,6 +123,46 @@ public class FmSynth {
 
 	}
 
+	private static class Inst1 implements Instrument {
+
+		private AudioOutput out;
+
+		private ADSR adsr;
+
+		public Inst1(AudioOutput out, double freq) {
+			super();
+			this.out = out;
+
+			Oscil lfo1 = new Oscil(200f, 200f, Waves.SQUARE);
+			Constant cons1 = new Constant(100f);
+			cons1.patch(lfo1.offset);
+			
+			Oscil lfo0 = new Oscil(0, 50f, Waves.SQUARE);
+			lfo1.patch(lfo0.frequency);
+			Constant cons0 = new Constant(f(freq));
+			cons0.patch(lfo0.offset);
+
+			Oscil toneOsc = new Oscil(0, 0.1f, Waves.SINE);
+			lfo0.patch(toneOsc.frequency);
+			
+			adsr = new ADSR(1f, 0.001f, 0.4f, 0.05f, 0.5f);
+			toneOsc.patch(adsr);
+		}
+
+		@Override
+		public void noteOn(float duration) {
+			adsr.noteOn();
+			adsr.patch(out);
+		}
+
+		@Override
+		public void noteOff() {
+			adsr.unpatchAfterRelease(out);
+			adsr.noteOff();
+		}
+
+	}
+
 	private static class Ctx {
 		private AudioOutput out;
 		private AudioRecorder rec;
