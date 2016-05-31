141930e6a5a0da4121af2429dee8aafaaad80ee6
wwagner4 <wwagner4@gmail.com>
2016-05-31 07:57:59 +0200

    More experiments with FmSynth

--- src/main/java/net/entelijan/tryout/FmSynth.java
+++ src/main/java/net/entelijan/tryout/FmSynth.java
@@ -8,6 +8,7 @@ import ddf.minim.*;
 import ddf.minim.javasound.JSMinim;
 import ddf.minim.spi.MinimServiceProvider;
 import ddf.minim.ugens.*;
+import ddf.minim.ugens.Noise.Tint;
 import net.entelijan.util.FileLoader;
 
 public class FmSynth {
@@ -62,7 +63,12 @@ public class FmSynth {
 	}
 
 	private void playNote(double time, double dur, double freq, Ctx ctx) {
-		Instrument i = new Inst0(ctx.out, freq);
+		Instrument i;
+		if (ran.nextBoolean()) {
+			i = new SteelDrum(ctx.out, freq);
+		} else {
+			i = new Quark(ctx.out, freq);
+		}
 		ctx.out.playNote(f(time), f(dur), i);
 	}
 
@@ -83,18 +89,24 @@ public class FmSynth {
 		}
 	}
 
-	private static class Inst0 implements Instrument {
+	private static class Quark implements Instrument {
 
 		private AudioOutput out;
 
 		private ADSR adsr;
+		private ADSR noiseAdsr;
 
-		public Inst0(AudioOutput out, double freq) {
+		public Quark(AudioOutput out, double freq) {
 			super();
 			this.out = out;
+			
+			Noise noise = new Noise(1f, Tint.RED);
+			Summer sum = new Summer();
+			noiseAdsr = new ADSR(0.04f, 0.2f, 0.2f, 0, 0.5f);
+			
 
-			Oscil lfo1 = new Oscil(f(freq / 3), 200f, Waves.SINE);
-			Constant cons1 = new Constant(f(freq / 50));
+			Oscil lfo1 = new Oscil(f(freq / 1), 200f, Waves.SINE);
+			Constant cons1 = new Constant(f(freq / 30));
 			cons1.patch(lfo1.offset);
 			
 			Oscil lfo0 = new Oscil(0, 50f, Waves.SINE);
@@ -102,16 +114,19 @@ public class FmSynth {
 			Constant cons0 = new Constant(f(freq));
 			cons0.patch(lfo0.offset);
 
-			Oscil toneOsc = new Oscil(0, 0.1f, Waves.TRIANGLE);
+			Oscil toneOsc = new Oscil(0, 0.4f, Waves.TRIANGLE);
 			lfo0.patch(toneOsc.frequency);
 			
-			adsr = new ADSR(1f, 0.001f, 0.15f, 0.05f, 0.5f);
-			toneOsc.patch(adsr);
+			adsr = new ADSR(0.6f, 0.02f, 0.15f, 0.1f, 0.5f);
+			noise.patch(noiseAdsr).patch(sum);
+			toneOsc.patch(sum);
+			sum.patch(adsr);
 		}
 
 		@Override
 		public void noteOn(float duration) {
 			adsr.noteOn();
+			noiseAdsr.noteOn();
 			adsr.patch(out);
 		}
 
@@ -119,17 +134,18 @@ public class FmSynth {
 		public void noteOff() {
 			adsr.unpatchAfterRelease(out);
 			adsr.noteOff();
+			noiseAdsr.noteOff();
 		}
 
 	}
 
-	private static class Inst1 implements Instrument {
+	private static class SteelDrum implements Instrument {
 
 		private AudioOutput out;
 
 		private ADSR adsr;
 
-		public Inst1(AudioOutput out, double freq) {
+		public SteelDrum(AudioOutput out, double freq) {
 			super();
 			this.out = out;
 
